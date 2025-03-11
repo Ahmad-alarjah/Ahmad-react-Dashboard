440ac53fe98b9f76c91394fbd9ce2cf1c2f33718
Ahmad Alarjah <160240167+Ahmad-Al3arja@users.noreply.github.com>
2025-03-11 16:34:50 +0200

    Done

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.mjs',  // Ensure this points to your PostCSS config file
+  },
 })
